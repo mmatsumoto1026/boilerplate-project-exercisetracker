a5b889b6b570afba0c1bb1074e48061b41b40b7c
mmatsumoto1026 <mmatsumoto1026@gmail.com>
2023-10-28 20:16:03 +0900

    fix: add packages and solve the project

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser')
 require('dotenv').config()
+
+const connectDB = require('./db/connect')
+const users = require('./routes/users')
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
 
 app.use(cors())
 app.use(express.static('public'))
+app.route('/api/users').post(bodyParser.urlencoded({extended: false}))
+app.route('/api/users/:_id/exercises').post(bodyParser.urlencoded({extended: false}));
+app.use('/api/users', users)
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
+const port = process.env.PORT || 3000;
 
+const listener = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+    console.log('Your app is listening on port ' + port)
+    })
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+listener();
